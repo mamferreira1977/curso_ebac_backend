0d95159da7ee22caa15bf96f8f78e9cb908e28e1
Marco Antonio <mamferreira@sga.pucminas.br>
2024-01-30 15:06:54 -0300

    Tarefa Nota Aluno
    Tarefa Nota Aluno

---
+++ src/Media.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Media {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        float nota1;
+        float nota2;
+        float nota3;
+        float nota4;
+        float nota;
+
+        System.out.println("Informe sua nota:");
+        nota1=ler.nextFloat();
+        System.out.println("Informe sua nota");
+        nota2=ler.nextFloat();
+        System.out.println("Informe sua nota");
+        nota3=ler.nextFloat();
+        System.out.println("Informe sua nota");
+        nota4=ler.nextFloat();
+
+        nota= (nota1+nota2+nota3+nota4)/4;
+        System.out.println("A média é " + nota);
+
+        if (nota >=7) {
+            System.out.println("Aluno Aprovado");
+        }
+        if (nota>=5 && nota<7) {
+            System.out.println("Aluno em Recuperação");
+        }
+        if (nota<5){
+        System.out.println("Aluno Reprovado");
+        }
+        }
+        }
+
