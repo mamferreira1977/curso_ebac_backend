9c636dde47a334b49523e2fbd098e3201af381dd
Marco Antonio <mamferreira@sga.pucminas.br>
2024-01-30 10:23:16 -0300

    Atividade Java
    Atividade Java classes

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Pessoa gente = new Pessoa("João", 25);
+        System.out.println(gente.getNome()+" " +gente.getIdade());
+    }
+}
---
+++ src/Matricula.java
@@ -0,0 +1,15 @@
+public class Matricula {
+
+    //Atributos da classe matricula
+    private String matricula;
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+
+}
